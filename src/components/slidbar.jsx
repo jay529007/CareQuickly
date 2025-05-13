74695622a89f6b1593874c8e645122c3cadea6b5
jay529007 <jaysatapara.3@gmail.com>
2025-05-13 16:17:56 +0530

    Update slidbar.jsx

--- src/components/slidbar.jsx
+++ src/components/slidbar.jsx
@@ -1,7 +1,15 @@
 import { FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { loadState } from "../store/localstorage";
 
 const Slidbar = ({ setSlidebarOpen }) => {
+  const authdata = loadState();
+  const type = authdata?.type || null;
+  const id = authdata?.id || null;
+
+  const isUser = type === "user";
+  const isAdmin = type === "admin";
+  const isDoctor = type === "doctor";
   const SLidebarButton =
     "block px-3 py-2 rounded text-black cursor-pointer hover:bg-blue-500 hover:text-white transition-all duration-100 ";
   return (
@@ -26,42 +34,99 @@ const Slidbar = ({ setSlidebarOpen }) => {
 
           {/* Menu Items */}
           <ul className="space-y-4">
-            <li>
-              <Link
-                to="/home"
-                onClick={() => setSlidebarOpen(false)}
-                className={SLidebarButton}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={() => setSlidebarOpen(false)}
-                to="/appointment/calendar"
-                className={SLidebarButton}
-              >
-                Appointments
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={() => setSlidebarOpen(false)}
-                to="/appointment/details"
-                className={SLidebarButton}
-              >
-                Appointment Details
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/profile"
-                onClick={() => setSlidebarOpen(false)}
-                className={SLidebarButton}
-              >
-                Profile
-              </Link>
-            </li>
+            {isUser && (
+              <>
+                <li>
+                  <Link
+                    to="/home"
+                    onClick={() => setSlidebarOpen(false)}
+                    className={SLidebarButton}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => setSlidebarOpen(false)}
+                    to="/appointment/calendar"
+                    className={SLidebarButton}
+                  >
+                    Appointments
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => setSlidebarOpen(false)}
+                    to="/appointment/details"
+                    className={SLidebarButton}
+                  >
+                    Appointment Details
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={`/doctor/profile/${id}`}
+                    onClick={() => setSlidebarOpen(false)}
+                    className={SLidebarButton}
+                  >
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
+            {isDoctor && (
+              <>
+                <li>
+                  <Link
+                    to="/doctor/dashboard"
+                    onClick={() => setSlidebarOpen(false)}
+                    className={SLidebarButton}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => setSlidebarOpen(false)}
+                    to="/doctor/appointments"
+                    className={SLidebarButton}
+                  >
+                    Appointment Details
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={`/doctor/profile/${id}`}
+                    onClick={() => setSlidebarOpen(false)}
+                    className={SLidebarButton}
+                  >
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
+            {isAdmin && (
+              <>
+                <li>
+                  <Link
+                    to="/patient/details"
+                    onClick={() => setSlidebarOpen(false)}
+                    className={SLidebarButton}
+                  >
+                    Patient Details
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => setSlidebarOpen(false)}
+                    to="/admin/dashboard"
+                    className={SLidebarButton}
+                  >
+                    Admin Dashboard
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
 
           {/* Footer Button */}
